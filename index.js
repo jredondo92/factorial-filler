a0034f3bb1121d71b48cbc91fc0d9cc27b5df191
Joaquim Redondo <joaquim.redondo@nicepeopleatwork.com>
2020-02-07 13:17:17 +0100

    Few tests and upgrades

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var jq = document.createElement('script');
 jq.src = "https://code.jquery.com/jquery-3.4.1.min.js";  /* Include any online jquery library you need */
-document.getElementsByTagName('head')[0].appendChild(jq);
+
+var jqui = document.createElement('script');
+jqui.src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+
+var css = document.createElement('link');
+css.setAttribute('rel', 'stylesheet');
+css.setAttribute('type', 'text/css');
+css.setAttribute('href', 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+
+document.getElementsByTagName('head')[0].appendChild(jq).appendChild(jqui).appendChild(css);
 
 function setNativeValue(element, value) {
     const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
@@ -14,7 +23,34 @@ function setNativeValue(element, value) {
     }
 }
 
-function openEverything ({ shouldSkipHoliday }) {
+const CATALAN = 'CATALAN'
+const SPANISH = 'SPANISH'
+const ENGLISH = 'ENGLISH'
+
+const LANG = {
+  SPANISH: {
+    friday: 'Viernes',
+    saturday: 'Sabado',
+    save: 'Submit',
+    startProcess: 'RELLENAR AUTOMATICAMENTE'
+  },
+  ENGLISH: {
+    friday: 'Friday',
+    saturday: 'Saturday',
+    save: 'Guardar',
+    startProcess: 'AUTO FILL'
+  },
+  CATALAN: {
+    friday: 'Divendres',    
+    saturday: 'Dissabte',
+    save: 'Desar',
+    startProcess: 'Iniciar pruses'
+  }
+}
+
+let selectedLanguage = LANG[ENGLISH]
+
+function openEverything ({ shouldSkipHoliday = true }) {
     $('tbody tr').each(async function (index, value) {
         var weekDay = $(this).find('[class*="weekDay"]')
         if (isWeekend(weekDay) || shouldSkipDay(this)) {
@@ -49,6 +85,44 @@ function shouldSkipDay(dayRow) {
     }
 }
 
+function addDialog() {
+  $('html').append(`<div id="dialog-confirm" title="Empty the recycle bin?">
+  <p><span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>These items will be permanently deleted and cannot be recovered. Are you sure?</p>
+</div>`)
+}
+
+function openDialog() {
+  $( "#dialog-confirm" ).dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      buttons: {
+        "Delete all items": function() {
+          $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+}
+
+function detectLanguage() {
+  switch ($('html').find('[class*="text"]').text().toUpperCase()) {
+    case 'HORAS TRABAJADAS':
+      selectedLanguage = LANG[SPANISH]
+      break;
+    case 'WORKED HOURS':
+      selectedLanguage = LANG[ENGLISH]
+      break;
+    case 'HORES TREBALLADES':
+      selectedLanguage = LANG[CATALAN]
+      break;
+  }
+
+}
+
 function changeValues() {
     $('tbody tr').each(async function (index, value) {
         var weekDay = $(this).find('[class*="weekDay"]')
@@ -79,8 +153,7 @@ function changeValues() {
 }
 
 function submitSchedules() {
-    $("button:contains('Submit')").each(function (index, value) {
+    $("button:contains("+LANG[selectedLanguage].save+")").each(function (index, value) {
         $(this).click()
     })
 }
-
